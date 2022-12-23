36f9230d09a40904b74dfd6af7e9e3b738cc3e90
mldamico <matias.lioneldamico@gmail.com>
2022-12-23 17:53:23 -0300

    Added buttons in expenses for load raw data and add expenses

--- app/routes/__app/expenses.tsx
+++ app/routes/__app/expenses.tsx
@@ -1,7 +1,8 @@
 import { LinksFunction } from "@remix-run/node";
-import { Outlet } from "@remix-run/react";
+import { Link, Outlet } from "@remix-run/react";
 import ExpensesList from "~/components/expenses/ExpensesList";
 import expensesStyles from "~/styles/expenses.css";
+import { FaPlus, FaDownload } from "react-icons/fa";
 const DUMMY_EXPENSES = [
   {
     id: "1",
@@ -21,6 +22,16 @@ const ExpensesLayout = () => {
     <>
       <Outlet />
       <main>
+        <section id="expenses-actions">
+          <Link to="add">
+            <FaPlus />
+            <span>Add Expense</span>
+          </Link>
+          <a href="/expenses/raw">
+            <FaDownload />
+            <span>Load Raw Data</span>
+          </a>
+        </section>
         <ExpensesList expenses={DUMMY_EXPENSES} />
       </main>
     </>
